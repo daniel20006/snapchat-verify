3ea2b496e14fa2277cdd45c543bad48afb1a9d50
Daniel Chima Okeke <danielokeke602@gmail.com>
2026-02-09 01:53:03 +0100

    Update redirect target URL in fetch function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
     // Paste your link from Phase 1 here
-    const target = "https://discussions-contributor-classic-forge.trycloudflare.com";
+    const target = "https://referring-wiring-winners-consultants.trycloudflare.com";
     return Response.redirect(target, 301);
   }
 }
